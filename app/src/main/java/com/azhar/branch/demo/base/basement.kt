6667ca26fdf2acda4182d7a9ec9521ed3d8c769b
Azhar Mahmood <azhar.mahmood@sprintingsoftware.com>
2023-10-26 17:58:36 +0500

    BaseFragment Added

--- app/src/main/java/com/azhar/branch/demo/base/basement.kt
+++ app/src/main/java/com/azhar/branch/demo/base/basement.kt
@@ -1,9 +1,28 @@
 package com.azhar.branch.demo.base
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 abstract class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
+
+abstract class BaseFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
