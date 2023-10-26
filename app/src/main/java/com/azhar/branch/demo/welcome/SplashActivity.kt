d017d12f5f0985253a907d4aabbda0453e9237a2
Azhar Mahmood <azhar.mahmood@sprintingsoftware.com>
2023-10-26 18:06:22 +0500

    splash_activity added

---
+++ app/src/main/java/com/azhar/branch/demo/welcome/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.azhar.branch.demo.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.azhar.branch.demo.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
